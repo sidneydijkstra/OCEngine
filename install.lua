e07c5da765b6cb215ffa7888e3701a2a1e634724
TheWiseSidney <sidney0099@hotmail.com>
2021-10-26 18:53:18 +0200

    Changed demo to demos

--- install.lua
+++ install.lua
@@ -51,6 +51,6 @@ if answer == "y" then
   shell.execute('mkdir /home/snake')
   for i, demo in ipairs(demos) do
     print(string.format("[OCEngine] %d/%d Downloading file %s", i, #demos, demo))
-    shell.execute(string.format('wget -fq "https://raw.githubusercontent.com/sidneydijkstra/OCEngine/main/demo/%s" "/home/%s"', demo, demo))
+    shell.execute(string.format('wget -fq "https://raw.githubusercontent.com/sidneydijkstra/OCEngine/main/demos/%s" "/home/%s"', demo, demo))
   end
 end
